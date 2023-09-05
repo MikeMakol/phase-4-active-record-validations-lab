781684a7f763a37d8882c3892281446eafa65388
Mike <mikemakol45@gmail.com>
2023-09-05 15:26:03 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :clickbait
+
+    CLICKBAITS = [ /Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i ]
+
+    def clickbait
+        errors.add(:title, "must be clickbait") unless CLICKBAITS.any? { |pat| pat.match(title)}
+    end
 end
